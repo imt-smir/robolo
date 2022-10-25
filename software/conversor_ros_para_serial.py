48ab7d4f2cc418c7c0a751f5188e4062dcdee6a2
João Vitor <21.01075-7@maua.br>
2022-10-25 11:44:19 -0300

    Cria funcao "sending_data"
    Repetição da funcionalidade de enviar a velocidade.
    
    Cria uma função no seguinte formato: sending_data(vel:float) -> None
    
    https://github.com/imt-smir/robolo/issues/5

--- software/conversor_ros_para_serial.py
+++ software/conversor_ros_para_serial.py
@@ -6,26 +6,22 @@ from geometry_msgs.msg import Twist
 
 serial_input = serial.Serial('/dev/ttyUSB0', 57600, timeout=1) 
 
+def sending_data(vel:float) -> None:
+    imax = 3
+    if(vel < 0.0):
+        imax = 4
+    for i in range(imax):
+        serial_input.write(bytes(str(vel)[i], 'utf-8'))
+    
+    print(str(vel)[0:imax], end='\t')
+
 def callback(data:Twist) -> None:
+    
     serial_input.write(b'ssss')
 
-
-    imax = 3
-    if(data.linear.x < 0.0):
-        imax = 4
-    for i in range(imax):
-        serial_input.write(bytes(str(data.linear.x)[i], 'utf-8'))
+    sending_data(data.linear.x)
+    sending_data(data.angular.z)
     
-    print(str(data.linear.x)[0:imax], end='\t');
-
-
-    imax = 3
-    if(data.angular.z < 0.0):
-        imax = 4
-    for i in range(imax):
-        serial_input.write(bytes(str(data.angular.z)[i], 'utf-8'))
-
-    print(str(data.angular.z)[0:imax]);
 
 
 def main():
