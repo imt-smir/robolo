c2f5247471533c3951269c586fd3e8ae88f811c7
João Vitor <21.01075-7@maua.br>
2022-10-27 11:26:37 -0300

    Cria função validateDataChunk
    https://github.com/imt-smir/robolo/issues/5

--- software/mapeamento/controle_do_motor/controle_do_motor.ino
+++ software/mapeamento/controle_do_motor/controle_do_motor.ino
@@ -34,49 +34,54 @@ void setup() {
 void loop() {
     c = mySerial.read();
     if(mySerial.available() > 0){
-        create_string(v, w);
-        changeVelocity();
+        data_not_lost = validateDataChunk(&data);
+        Serial.print(data);
+        Serial.print('\t');
+        Serial.println(data_not_lost);
+        // readingdataChink(char* v, char* w);
+        // stringToVel(char* v, char* w, float* x, float* z);
+        // updateSetpoint(float x, float z);
     }
 
-    //Criando um intervalo para a mudança da velocidade do motor
-    if (millis() - counter > ENCODER_READING_TIME) {
-        // Cálculo do tempo
-        tf = millis(); // em milissegundo
-        dt = ((float) (tf - to)) / (1000.0); // em segundos
-        to = tf; // em milissegundo
+    // //Criando um intervalo para a mudança da velocidade do motor
+    // if (millis() - counter > ENCODER_READING_TIME) {
+    //     // Cálculo do tempo
+    //     tf = millis(); // em milissegundo
+    //     dt = ((float) (tf - to)) / (1000.0); // em segundos
+    //     to = tf; // em milissegundo
 
-        // Cálculo da velocidade
-        last_velocity1  = velocity1; // em rotações por segundo
-        velocity1  = (encoder_data1 - last_encoder_data1)/ (300.0 * dt);   // 300 pulsos por volta, portanto isto transforma
-                                                                // pulsos por segundo em rotações por segundo
-        last_encoder_data1  = encoder_data1; // em rotações por segundo
+    //     // Cálculo da velocidade
+    //     last_velocity1  = velocity1; // em rotações por segundo
+    //     velocity1  = (encoder_data1 - last_encoder_data1)/ (300.0 * dt);   // 300 pulsos por volta, portanto isto transforma
+    //                                                             // pulsos por segundo em rotações por segundo
+    //     last_encoder_data1  = encoder_data1; // em rotações por segundo
 
-        last_velocity2 = velocity2;
-        velocity2 = (encoder_data2 - last_encoder_data2) / (300.0 * dt);
-        last_encoder_data2 = encoder_data2;
+    //     last_velocity2 = velocity2;
+    //     velocity2 = (encoder_data2 - last_encoder_data2) / (300.0 * dt);
+    //     last_encoder_data2 = encoder_data2;
 
-        // Cálculo do erro
-        last_e1 = e1;
-        e1 = setpoint_vel1 - velocity1;
+    //     // Cálculo do erro
+    //     last_e1 = e1;
+    //     e1 = setpoint_vel1 - velocity1;
 
-        last_e2 = e2;
-        e2 = setpoint_vel2 - velocity2;
+    //     last_e2 = e2;
+    //     e2 = setpoint_vel2 - velocity2;
 
-        // Aplicando a equação de diferenças encontrada
-        pwm1 = 11.05 * e1 + 1.093 * last_e1 + pwm1;
-        pwm2 = 9.247 * e2 + 2.608 * last_e2 + pwm2;
+    //     // Aplicando a equação de diferenças encontrada
+    //     pwm1 = 11.05 * e1 + 1.093 * last_e1 + pwm1;
+    //     pwm2 = 9.247 * e2 + 2.608 * last_e2 + pwm2;
 
-        if (pwm1 >= 255) pwm1 = 255; // pwmMáx = 255
-        if (pwm1 <= -255) pwm1 = -255;
+    //     if (pwm1 >= 255) pwm1 = 255; // pwmMáx = 255
+    //     if (pwm1 <= -255) pwm1 = -255;
 
-        if (pwm2 >= 255) pwm2 = 255; // pwmMáx = 255
-        if (pwm2 <= -255) pwm2 = -255;
+    //     if (pwm2 >= 255) pwm2 = 255; // pwmMáx = 255
+    //     if (pwm2 <= -255) pwm2 = -255;
 
 
-        // Atualizando o movimento do motor
-        rotateMotor(pwm1, pwm2);
+    //     // Atualizando o movimento do motor
+    //     rotateMotor(pwm1, pwm2);
 
-        // Próxima iteração do "void loop()" não entrará novamente neste while, apenas quando o novo intervalo chegar
-        counter = millis();
-    }
+    //     // Próxima iteração do "void loop()" não entrará novamente neste while, apenas quando o novo intervalo chegar
+    //     counter = millis();
+    // }
 }
--- software/mapeamento/controle_do_motor/motor.ino
+++ software/mapeamento/controle_do_motor/motor.ino
@@ -38,58 +38,69 @@ void rotateMotor(int pwm1, int pwm2){
 }
 
 // Recebe os valores lidos da entrada serial, e transforma em uma sequência de char 
-void create_string(char* v, char* w){
-    int idx = 0;
-    int i = 3; 
-    data = mySerial.readString();
-    while(data[idx] == 's'){
-        idx++;
-    }
-    // Velocidade linear
-    v[0] = data[idx];
-    idx++;
-    if(v[0] == '-'){
-        i++;
-    }
-    for(int k = 1; k < i; k++){
-        v[k] = data[idx];
-        idx++;
-    }
-    v[i] = '\0';
+// void create_string(char* v, char* w){
+//     int idx = 0;
+//     int i = 3; 
+//     data = mySerial.readString();
+//     while(data[idx] == 's'){
+//         idx++;
+//     }
+//     // Velocidade linear
+//     v[0] = data[idx];
+//     idx++;
+//     if(v[0] == '-'){
+//         i++;
+//     }
+//     for(int k = 1; k < i; k++){
+//         v[k] = data[idx];
+//         idx++;
+//     }
+//     v[i] = '\0';
 
-    // Velocidade angular
-    i = 3;
-    w[0] = data[idx];
-    idx++;
-    if(w[0] == '-'){
-        i++;
-    }
-    for(int k = 1; k < i; k++){
-        w[k] = data[idx];
-        idx++;
-    }
-    w[i] = '\0';
-}
+//     // Velocidade angular
+//     i = 3;
+//     w[0] = data[idx];
+//     idx++;
+//     if(w[0] == '-'){
+//         i++;
+//     }
+//     for(int k = 1; k < i; k++){
+//         w[k] = data[idx];
+//         idx++;
+//     }
+//     w[i] = '\0';
+// }
+
+
+
 
 // Altera as velocidades desejadas conforme os controles no teleop
-void changeVelocity(){
-    x = atof(v);
-    z = atof(w);
+// void changeVelocity(){
+//     x = atof(v);
+//     z = atof(w);
     
 
-    setpoint_vel1 = (2 * x - z * WHEEL_DISTANCE) * RAD_TO_ROT / (2 * WHEEL_RADIUS);;
-    if (setpoint_vel1 < -MAX_VEL){
-        setpoint_vel1 = -MAX_VEL;
-    }
-    else if(setpoint_vel1 > MAX_VEL){
-        setpoint_vel1 = MAX_VEL;
-    }
+//     setpoint_vel1 = (2 * x - z * WHEEL_DISTANCE) * RAD_TO_ROT / (2 * WHEEL_RADIUS);;
+//     if (setpoint_vel1 < -MAX_VEL){
+//         setpoint_vel1 = -MAX_VEL;
+//     }
+//     else if(setpoint_vel1 > MAX_VEL){
+//         setpoint_vel1 = MAX_VEL;
+//     }
 
-    setpoint_vel2 = (2 * x + z * WHEEL_DISTANCE) * RAD_TO_ROT / (2 * WHEEL_RADIUS);
-    if (setpoint_vel2 < -MAX_VEL){
-        setpoint_vel2 = -MAX_VEL;
-    }
-    else if(setpoint_vel2 > MAX_VEL){
-        setpoint_vel2 = MAX_VEL;
-    }
+//     setpoint_vel2 = (2 * x + z * WHEEL_DISTANCE) * RAD_TO_ROT / (2 * WHEEL_RADIUS);
+//     if (setpoint_vel2 < -MAX_VEL){
+//         setpoint_vel2 = -MAX_VEL;
+//     }
+//     else if(setpoint_vel2 > MAX_VEL){
+//         setpoint_vel2 = MAX_VEL;
+//     }
+// }
+
+bool validateDataChunk(String* data){
+    *data = mySerial.readString();
+    if((*data)[0] != '\x05')  return false;
+    if((*data)[(*data).length() - 1] != '\x04') return false;
+    if(atoi((*data)[(*data).length() - 2]) != (*data).length() - 3) return false;
+    return true;
 }
